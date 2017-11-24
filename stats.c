ea6c91c2b5fdbd28f0f81ccbb628445886a6b35d
Gordon <agyngo77@gmail.com>
2017-11-24 00:40:48 -0700

    Adding code defination for C1M1 Assessment

--- stats.c
+++ stats.c
@@ -38,7 +38,16 @@ void main() {
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};   
+                                7,  87, 250, 230,  99,   3, 100,  90}; 
+/* call the data set */
+  sort_array( test, sizeof(test)/sizeof(unsigned char) );  
+
+/* print data set to the screen */
+ 
+  print_array( test, sizeof(test)/sizeof(unsigned char) ); 
+ 
+/* print staticts of the data set */
+  print_statistics( test, sizeof(test)/sizeof(unsigned char) );  
   
  }
 
@@ -53,7 +62,11 @@ void main() {
 	void 							  		   *
  ***********************************************************************************/ 
  void print_array(unsigned char *data, unsigned int array_size){
-   
+    if( data == NULL)
+	return ;
+    for( unsigned int i = 0; i < array_size; i++)
+       printf("%u ", data[i]);
+    printf("\n");
   }
 
 /******************************************************************************************
@@ -69,7 +82,13 @@ void main() {
 	void					    					  *
  ******************************************************************************************/   
  void print_statistics(unsigned char *data, unsigned int array_size) {
-      
+    if( data == NULL)
+	return ;
+    printf("--------------------------------------\n");
+    printf("| MINIMUM | MAXIMUM | MEAN  | MEDIAN |\n");
+    printf("-------------------------------------\n");
+    printf("| %4u    | %4u    | %5.2f | %5.2f  |\n" ,find_minimum(data, array_size ) ,find_maximum(data, array_size),find_mean ( data, array_size ), find_median( data, array_size ) );
+    printf("--------------------------------------\n");   
  }
 
 /*************************************************************************************
@@ -83,7 +102,14 @@ void main() {
 	Median of the numbers in the array					     *
  *************************************************************************************/ 
  float find_median( unsigned char *data, unsigned int array_size ) {
-   
+    if( data == NULL)
+	return 0;
+    float median = 0.00;
+    if( (array_size % 2) == 0 ) 
+	median = (float)((data[ array_size/2 ]) +(data[ (array_size/2) -1]))/2; // median for even number array size 
+    else
+	median = data[ array_size/2 ] ; //  median for odd number array size
+    return median;
   }
 
 
@@ -98,7 +124,13 @@ void main() {
 	Mean of the numbers in the array					     	*
  ****************************************************************************************/ 
  float find_mean( unsigned char *data, unsigned int array_size){
-    
+    if( data == NULL)
+	return 0.00;
+    float mean, sum = 0;
+    for( unsigned int i = 0; i < array_size; i++)
+       sum += data[i];
+    mean = sum/array_size;
+    return mean; 
  }
 
 /****************************************************************************************
@@ -113,6 +145,14 @@ void main() {
 	Maximum of the numbers in the array					     	*
  ****************************************************************************************/ 
  unsigned char find_maximum(unsigned char *data, unsigned int array_size){
+    if( data == NULL)
+	return 0;
+    unsigned char maximum = data[0];
+    for( unsigned int i = 1; i < array_size; i++){
+       if( maximum < data[i] )
+          maximum = data[i];
+    }
+    return maximum;
     
  }
 
@@ -128,7 +168,14 @@ void main() {
 	Minimum of the numbers in the array					     	*
  ****************************************************************************************/
  unsigned char find_minimum(unsigned char *data, unsigned int array_size){
-  
+   if( data == NULL)
+	return 0;
+   unsigned char minimum = data[0];
+    for( unsigned int i = 1; i < array_size; i++){
+       if( data[i] < minimum)
+          minimum = data[i];
+    }
+  return minimum;
  }
  
 /*******************************************************************************************
@@ -142,6 +189,15 @@ void main() {
 	void					     	          			   *
  *******************************************************************************************/ 
   void sort_array( unsigned char *data, unsigned int array_size ) {
-  
+    if( data == NULL)
+	return ;
+    for( unsigned int i = 1, j; i < array_size; i++){
+        unsigned char tmp = data[i];
+        for( j = i; j > 0 && tmp > data[j-1]; j--)
+            data[j] = data[j-1];
+        data[j] = tmp;
+        }
    }
     
+  
+
